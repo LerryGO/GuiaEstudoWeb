f92e76031f4ba68cce319ce514037ff8cde7737c
LerryGO <lerry.pirani@gmail.com>
2020-08-22 10:24:15 +0000

    (22/08/20) Inserindo exemplificação de comentários

---
+++ comentario.js
@@ -0,0 +1,20 @@
+//Comentário de uma linha
+console.log('Linha 1')
+
+/*
+Comentário de
+múltiplas
+ linhas
+*/
+console.log('Linha 2')
+
+/*  
+ * comentário
+ * de multiplas
+ * linhas
+ * (Apenas inserir o * que ele fará o resto)
+ * 
+ * 
+ */
+console.log('Linha 3')
+ 
