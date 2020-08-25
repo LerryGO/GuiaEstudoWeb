8cf587b5d8f0f5c5f2e31f1e3de33d2878efaed2
LerryGO <lerry.pirani@gmail.com>
2020-08-25 17:41:06 +0000

    (25/08/20) O básico do Var, Let e const

---
+++ Basico_var_let_const.js
@@ -0,0 +1,25 @@
+//PALAVRAS RESERVADAS PARA ATRIBUIÇÃO DE VALORES
+
+
+//Atribuindo valor a variável
+var a = 3 // Evite usar o 'var' (forma antiga)
+let b = 4 //Forma mais moderna de criar variavel
+
+/* Usando o 'Var' é possível ser redeclarada a mesma variavel!
+ * O 'Let' não deixa com que seja redeclarado a mesma variavel, instigando a um 'Erro' e isso ajuda muito
+ * a não deixar 'apagar' sem querer qualquer tipo de variavel ou informação
+ */
+
+//Reatribuição de valores
+a = 30
+b = 40
+
+//---------------------------------
+
+//Constante
+const c = 5 //Uma constante não pode ser ALTERADA!
+//Tudo que não necessita ou não tem motivo para ser alterado, é super indicado que sempre se use uma Constante!
+
+//Obs.: Sempre evite ficar alterando os valores das variaveis, procure atribuir valores a variaveis diferentes.
+
+
